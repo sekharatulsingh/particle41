1953c9cfacba6ff5f1612d307217c236f29cdd6e
Atul Singh <73229173+sekharatulsingh@users.noreply.github.com>
2025-12-11 15:04:41 +0530

    Create sg_ecs_tasks.tf

---
+++ infra/sg_ecs_tasks.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "ecs_tasks" {
+  name   = "ecs-tasks-sg"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 8080
+    to_port         = 8080
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
